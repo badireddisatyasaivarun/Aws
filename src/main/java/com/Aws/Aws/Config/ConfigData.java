ebdfe2bd09c0714980250a2a2927ddea0d59ab98
badireddisatyasaivarun <41924086+badireddisatyasaivarun@users.noreply.github.com>
2021-05-17 15:14:07 +0530

    Changed Keys

--- src/main/java/com/Aws/Aws/Config/ConfigData.java
+++ src/main/java/com/Aws/Aws/Config/ConfigData.java
@@ -4,8 +4,8 @@ import lombok.Data;
 
 public @Data class ConfigData {
 
-	public static String accessKey = "AKIA242BACD2JKE67DX5";
-	public static String accessSecret = "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";
+	public static String accessKey = "AKIA242BACD2JKE67FX5";
+	public static String accessSecret = "Hq2YqS/J8kav98fPh/FYmYj+XDNouhzr";
 	public static String region = "ap-south-1";
 	public static String buketName = "varun-bucket-001";
 }
